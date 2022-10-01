a8517a6270271daccc781e795ca267aae219d64a
Ryan Lee <lee98064@gmail.com>
2022-10-01 21:15:24 +0800

    Add admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminLayout',
+}
+</script>
+
+<style scoped>
+* {
+  font-family: 'Noto Sans TC', sans-serif;
+  font-weight: 600;
+}
+html {
+  overflow-x: hidden;
+}
+body {
+  overflow-x: hidden;
+  background-color: #eeeeee;
+}
+</style>
